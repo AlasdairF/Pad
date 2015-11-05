68f9c7f8c78d3ab81890ee3e4320a9d9a281f7bc
AlasdairF <AlasdairF@users.noreply.github.com>
2015-11-05 15:18:28 +0700

    Added StringBytes

--- pad.go
+++ pad.go
@@ -35,3 +35,21 @@ func String(input string, to int, padder byte) string {
 
 	return string(b)
 }
+
+func StringBytes(input string, to int, padder byte) []byte {
+
+	l := len(input)
+	if l >= to {
+		return input
+	}
+	
+	b := make([]byte, to)
+	dif := to - l
+	var i int
+	for i=0; i<dif; i++ {
+		b[i] = padder
+	}
+	copy(b[i:], input)
+
+	return b
+}
